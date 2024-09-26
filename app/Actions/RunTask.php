dc77e63737c637bd862b70a1ea66f21052514b5b
Easton Meth <easton.meth@gmail.com>
2024-09-26 17:44:48 -0400

    Refactor

--- app/Actions/RunTask.php
+++ app/Actions/RunTask.php
@@ -16,25 +16,27 @@ class RunTask
      */
     public function handle(int $taskId): void
     {
-        $task = Task::find($taskId);
-        if (! $task) {
-            throw new Exception('Task not found');
-        }
+        $task = Task::findOrFail($taskId);
         $server = $task->server;
+
         if (! $server) {
             throw new Exception('Server not found');
         }
+
         $credential = $task->serverCredential;
+
         if (! $credential) {
             throw new Exception('Server credential not found');
         }
+
         $key = PublicKeyLoader::load($credential->ssh_private_key, $credential->passphrase);
         $ssh = new SSH2($server->hostname);
+
         if (! $ssh->login($credential->username, $key)) {
             throw new Exception('Login failed');
         }
 
-        $run = new Run();
+        $run = new Run;
         $run->tenant_id = $task->tenant->id;
         $run->task_id = $task->id;
         $run->status = RunStatus::RUNNING;
