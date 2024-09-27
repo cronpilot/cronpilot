664078d12f2d2d408dd23023e7cceec4e374e87e
Eben Casely-Hayford <ecaselyhayford@givelify.com>
2024-09-27 10:52:26 +0000

    Task-33: fixed typo

--- database/migrations/2024_09_27_100548_add_pause_field_to_tasks_table.php
+++ database/migrations/2024_09_27_100548_add_pause_field_to_tasks_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->boolean('pause')->default(false);
+            $table->boolean('paused')->default(false);
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->dropColumn('pause');
+            $table->dropColumn('paused');
         });
     }
 };
