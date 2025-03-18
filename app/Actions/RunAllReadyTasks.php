e8b33ee1a014ea2f1967e6945d11331ba9e00ca6
Peter Meth <pmeth@delvia.com>
2025-03-18 12:57:32 -0400

    Do not stop on errors when running all tasks (#53)

--- app/Actions/RunAllReadyTasks.php
+++ app/Actions/RunAllReadyTasks.php
@@ -4,15 +4,17 @@ namespace App\Actions;
 
 use App\Enums\TaskStatus;
 use App\Models\Task;
+use Illuminate\Support\Facades\Log;
 
 class RunAllReadyTasks
 {
     public function __invoke(): void
     {
+        Log::info('Running all ready tasks');
         $tasks = Task::readyToRun()->get();
 
-
         foreach ($tasks as $task) {
+            Log::info("Running task: {$task->id}");
             (new RunTask())->handle($task->id);
         }
     }
--- app/Actions/RunTask.php
+++ app/Actions/RunTask.php
@@ -6,8 +6,10 @@ use App\Enums\RunStatus;
 use App\Models\Run;
 use App\Models\Task;
 use Exception;
+use Illuminate\Support\Facades\Log;
 use phpseclib3\Crypt\PublicKeyLoader;
 use phpseclib3\Net\SSH2;
+use Throwable;
 
 class RunTask
 {
@@ -16,53 +18,66 @@ class RunTask
      */
     public function handle(int $taskId): void
     {
-        $task = Task::findOrFail($taskId);
-        $server = $task->server;
-
-        if (! $server) {
-            throw new Exception('Server not found');
-        }
-
-        $credential = $task->serverCredential;
-
-        if (! $credential) {
-            throw new Exception('Server credential not found');
-        }
-
-        $key = $credential->passphrase
-            ? PublicKeyLoader::load($credential->ssh_private_key, $credential->passphrase)
-            : PublicKeyLoader::load($credential->ssh_private_key);
-        $ssh = new SSH2($server->hostname, $server->ssh_port);
-
-        if (! $ssh->login($credential->username, $key)) {
-            throw new Exception('Login failed');
+        $task = Task::find($taskId);
+        if (! $task) {
+            Log::warning("Task not found: {$taskId}");
+            return;
         }
 
         $run = new Run;
         $run->tenant_id = $task->tenant->id;
         $run->task_id = $task->id;
         $run->status = RunStatus::RUNNING;
+        $run->output = '';
         $run->duration = 0;
         $run->save();
         $start = now();
 
-        $output = $ssh->exec($task->command);
-        $exitStatus = $ssh->getExitStatus();
+        try {
+            $server = $task->server;
+            if (! $server) {
+                throw new Exception('Server not found');
+            }
 
-        $run->duration = $start->diffInSeconds(now());
-        $run->output = $output;
-        if ($exitStatus === false) {
+            $credential = $task->serverCredential;
+
+            if (! $credential) {
+                throw new Exception('Server credential not found');
+            }
+
+            $key = $credential->passphrase
+                ? PublicKeyLoader::load($credential->ssh_private_key, $credential->passphrase)
+                : PublicKeyLoader::load($credential->ssh_private_key);
+            $ssh = new SSH2($server->hostname, $server->ssh_port);
+
+            if (! $ssh->login($credential->username, $key)) {
+                throw new Exception('Login failed');
+            }
+
+            $output = $ssh->exec($task->command);
+            $exitStatus = $ssh->getExitStatus();
+
+            $run->duration = $start->diffInSeconds(now());
+            $run->output = $output;
+            if ($exitStatus === false) {
+                $run->status = RunStatus::FAILED;
+                $run->output .= "\n[ERROR] Unable to determine exit status.";
+            } elseif ($exitStatus !== 0) {
+                $run->status = RunStatus::FAILED;
+                $run->output .= "\n[ERROR] Command failed with exit status: {$exitStatus}";
+            } else {
+                $run->status = RunStatus::SUCCESSFUL;
+            }
+        } catch (Throwable $e) {
             $run->status = RunStatus::FAILED;
-            $run->output .= "\n[ERROR] Unable to determine exit status.";
-        } elseif ($exitStatus !== 0) {
-            $run->status = RunStatus::FAILED;
-            $run->output .= "\n[ERROR] Command failed with exit status: {$exitStatus}";
-        } else {
-            $run->status = RunStatus::SUCCESSFUL;
+            if ($run->output !== '') {
+                $run->output .= "\n";
+            }
+            $run->output .= "[ERROR] {$e->getMessage()}";
         }
+
         $run->save();
 
-        // @todo: this is just fake for now. Implement properly once we have the rrules figured out
         $task->scheduleNextRun(now());
         $task->save();
     }
